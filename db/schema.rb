b5417d4d11d9739ddd01ee32beaf55a01d8c849f
Duy Nguyen <quangduyx188@gmail.com>
2015-10-20 17:06:12 +0700

    signup_login_logout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151019090919) do
+ActiveRecord::Schema.define(version: 20151020095412) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "target_id"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 20151019090919) do
     t.string   "remember_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   create_table "words", force: :cascade do |t|
