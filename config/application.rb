2b9d1d0965f865f432aaf0a712f2ad4cc15e0cfd
nghicv <nghicv.57@gmail.com>
2015-10-26 10:24:27 +0700

    make profile

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module BaseApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    Bundler.require(*Rails.groups)
+    Config::Integration::Rails::Railtie.preload
   end
 end
