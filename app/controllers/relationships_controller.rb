1107eee1e0f88b33be37098aa637c1ec885bcac2
nghicv <nghicv.57@gmail.com>
2015-10-28 17:04:16 +0700

    make-his-summary

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,31 @@
+class RelationshipsController < ApplicationController
+
+  def index
+    follow = params[:type]
+    @user  = User.find params[:id]
+    if Settings.follow_type.include? follow
+      @title = t follow
+      @users = @user.send(follow).paginate page: params[:page]
+    else
+      redirect_to @user
+    end
+  end
+
+  def create
+    @user = User.find params[:followed_id]
+    current_user.follow @user
+    respond_to do |format|
+      format.html{redirect_to @user}
+      format.js
+    end
+  end
+
+  def destroy
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow @user
+    respond_to do |format|
+      format.html{redirect_to @user}
+      format.js
+    end
+  end
+end
